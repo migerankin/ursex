07cee6b9af19184d951ef0f14ca891a5cf8dc209
migerankin <244283586@qq.com>
2023-10-04 14:36:40 +0800

    ursex

---
+++ js/main.js
@@ -0,0 +1,127 @@
+
+const urlstringM = [
+    {id:'19570636',name:'なよし',kind:'fanbox'},
+    {id:'66210',name:'村上',kind:'fanbox'},
+    {id:'6668233',name:'司马ゆうじ',kind:'fanbox'},
+    {id:'53007605',name:'宏き',kind:'fanbox'},
+    {id:'10145955',name:'zifu',kind:'fanbox'},
+    {id:'73530687',name:'nullQ',kind:'fanbox'},
+    {id:'10397209',name:'吾朗ナオキ',kind:'fanbox'},
+    {id:'52323456',name:'さか',kind:'fanbox'},
+    {id:'598317',name:'ソウスケ',kind:'fanbox'},
+    {id:'10114626',name:'ぎんま',kind:'fanbox'},
+    {id:'79281725',name:'小白天',kind:'fanbox'},
+    {id:'17225998',name:'Aminor',kind:'fanbox'},
+    {id:'31468963',name:'はしくれ太郎',kind:'fanbox'},
+    {id:'14748801',name:'ΛＪ',kind:'patreon'},
+    {id:'44114364',name:'聖志',kind:'fanbox'},
+    {id:'29151559',name:'イズ',kind:'fanbox'},
+    {id:'383014',name:'エイチ',kind:'fanbox'},
+    {id:'49249736',name:'alt*',kind:'fanbox'},
+    {id:'2722',name:'funa',kind:'fanbox'},
+    {id:'4606746',name:'bttn',kind:'fanbox'},
+    {id:'3656682',name:'Bludwing',kind:'patreon'},
+    {id:'58627622',name:'vinceteaa',kind:'patreon'},
+]
+const urlstringP = [
+    {id:'chaptar_F',name:'chapterF',iconId:'1679450223415033857/XjPFRBgw',barId:'1625533498865094658/1680368943',isBar:true},
+    {id:'adoniself',name:'Adoniself',iconId:'1679915796200574978/Q1Wa8teb',barId:'1115964587512197120/1681376684',isBar:true},
+    {id:'kreem_6',name:'野男人',iconId:'1689705274603618309/h_0Ml814',barId:'949658839/1691928302',isBar:true},
+    {id:'Liability_QS',name:'Liability_QS',iconId:'1507598564473466880/HepqqJEy',barId:'758692405944131584/1566873295',isBar:true},
+    {id:'lukun2021',name:'陆堃',iconId:'1543424387612700674/ysMGzc0u',barId:'758692405944131584/1566873295',isBar:false},
+    {id:'mrn00n',name:'mrnono',iconId:'1362388575904403462/gZTLjie5',barId:'1362388414763425792/1665724453',isBar:true},
+    {id:'PK_MuscleEdge',name:'P.K Edge',iconId:'1454754117427941387/y1uaSn0M',barId:'1451797050530545667/1635675494',isBar:true},
+    {id:'Public_Y_naked',name:'野裸回歸',iconId:'1626536385812520960/dpzpRZ4B',barId:'1543919263983206404/1656935028',isBar:true},
+    {id:'rushdick1',name:'Rush Dick',iconId:'1505980550166761477/Ph08e4mT',barId:'3959100793/1605695581',isBar:true},
+    {id:'alan4136',name:'深圳精🐮1',iconId:'1655479439390818304/6mXgNu4S',barId:'1386687391566532611/1679468558',isBar:true},
+    {id:'AuRevoir_178',name:'Au Revoir',iconId:'1147987838572433408/LjMdLL0R',barId:'1104717922130288640/1692476898',isBar:true},
+    {id:'MentoMensex',name:'MentoMen Porn',iconId:'1661978396845572097/0lcIJSFu',barId:'972712045496827904/1685080774',isBar:true},
+    {id:'laohuang1069',name:'隔壁老黄',iconId:'1600897740157857792/qXUwd6XP',barId:'FwkFHoRaMAEO1Kg?format=jpg&name=large',isBar:false},
+    {id:'hz_shengfu',name:'绳缚',iconId:'1500679138759168000/lBSBiFED',barId:'1056510172095369216/1649915329',isBar:true},
+    {id:'MuMuL422',name:'穆海L',iconId:'1474160196746244108/MRFCGFrQ',barId:'1473336990447792129/1647274136',isBar:true},
+    {id:'xiaoqiuyin666',name:'请不要叫我小蚯蚓',iconId:'1393772614900871171/z8Aj6q8Q',barId:'F2hiUpMbcAAmbXa?format=jpg&name=large',isBar:false},
+    {id:'sudigege',name:'苏底哥哥',iconId:'1676345543705120768/sfjcYx8K',barId:'1633703535052472320/1678340899',isBar:true},
+    {id:'Archy400',name:'Archy400',iconId:'1587119472531210243/qjk0qz6s',barId:'1582755563376504833/1687929333',isBar:true},
+    {id:'KuangKu57395139',name:'肉便犬育成訓練',iconId:'1205463349284073478/7ra7tkIF',barId:'1055506212551188481/1622741941',isBar:true},
+    {id:'Daniel88263261',name:'Daniel武汉',iconId:'1559571953182441472/_JUwLIyz',barId:'1055506212551188481/1622741941',isBar:true},
+    {id:'Exposedxin',name:'Xin',iconId:'1536283110882410496/BaDqEa7m',barId:'497491010/1620318299',isBar:true},
+    {id:'MuMuL422',name:'穆海L',iconId:'1474160196746244108/MRFCGFrQ',barId:'1473336990447792129/1647274136',isBar:true},
+    {id:'holdck808',name:'L4X',iconId:'1553944671683940352/q7n26dCb',barId:'F5LJwiqb0AAZRSd?format=jpg&name=large',isBar:false},
+    {id:'CUYEEBLACK',name:'粗野小黑',iconId:'1616013845092536320/cuk0LMPH',barId:'F5n28zrakAAvivJ?format=jpg&name=4096x4096',isBar:false},
+    {id:'Muscletool',name:'阿超',iconId:'1662315659123122180/fWXCnE-g',barId:'901380100792487936/1685247730',isBar:true},
+    {id:'margoo94440147',name:'margoo',iconId:'1620352496232468480/nAFqVlVh',barId:'1105378721941270528/1659165402',isBar:true},
+    {id:'Dark_skiinmode',name:'Darkskm',iconId:'1539320038687068162/aNEtNsdm',barId:'925084055544086529/1609474682',isBar:true},
+    {id:'ANILMEENA20000',name:'鲍林吃不饱',iconId:'1607935426647539713/--Mv3_9e',barId:'925084055544086529/1609474682',isBar:false},
+    {id:'nzkmhxevjmizkn',name:'鹿sir',iconId:'1433682960121663488/dle-Dy-U',barId:'925084055544086529/1609474682',isBar:false},
+    {id:'POLOPLUTO1',name:'POLOPLUTO',iconId:'1006382388254072832/YVFp5ggu',barId:'1006376537157877761/1528775263',isBar:true},
+    {id:'Xingxing419',name:'星星队长',iconId:'1701894800528830464/bUqGEdU4',barId:'1006376537157877761/1528775263',isBar:false},
+    {id:'LouisTheDeka',name:'Louis🐮',iconId:'1526352412206051328/Hsu_F8p4',barId:'1014062044713111552/1610085223',isBar:true},
+    {id:'cute_prince2023',name:'可愛小王子',iconId:'1696717371673821184/7XtaLEgI',barId:'1668490437726978048/1686992285',isBar:true},
+
+]
+choiseP()
+function choiseP(){
+    document.getElementsByClassName('box')[0].innerHTML = ``
+    for(let i = 0; i < urlstringP.length; i++){
+        if(urlstringP[i].isBar==true){
+        document.getElementsByClassName('box')[0].innerHTML += `
+        <a href="https://twitter.com/${urlstringP[i].id}/media" target="_blank">
+            <div style="background: linear-gradient(rgba(0, 0, 0, 0.3), rgba(0, 0, 0, 0.65)), url('https://pbs.twimg.com/profile_banners/${urlstringP[i].barId}/1500x500');background-size:auto 100%;background-position: center center;" class="bar">
+                <div class="icon">
+                    <img src="https://pbs.twimg.com/profile_images/${urlstringP[i].iconId}_400x400.jpg" class="iconIMG">
+                </div>
+                <div>
+                    <div class="name">${urlstringP[i].name}</div>
+                </div>
+            </div>
+        </a>`}
+        else{
+            document.getElementsByClassName('box')[0].innerHTML += `
+        <a href="https://twitter.com/${urlstringP[i].id}/media" target="_blank">
+            <div style="background-color:#333639;" class="bar">
+                <div class="icon">
+                    <img src="https://pbs.twimg.com/profile_images/${urlstringP[i].iconId}_400x400.jpg" class="iconIMG">
+                </div>
+                <div>
+                    <div class="name">${urlstringP[i].name}</div>
+                </div>
+            </div>
+        </a>`
+        }
+    }}
+function choiseM(){
+document.getElementsByClassName('box')[0].innerHTML = ``
+for(let i = 0; i < urlstringM.length; i++){
+    document.getElementsByClassName('box')[0].innerHTML += `
+    <a href="https://kemono.party/${urlstringM[i].kind}/user/${urlstringM[i].id}" target="_blank">
+        <div style="background: linear-gradient(rgba(0, 0, 0, 0.3), rgba(0, 0, 0, 0.65)), url('https://img.kemono.party/banners/${urlstringM[i].kind}/${urlstringM[i].id}');background-position: center center;" class="bar">
+            <div class="icon">
+                <img src="https://img.kemono.party/icons/${urlstringM[i].kind}/${urlstringM[i].id}" class="iconIMG">
+            </div>
+            <div>
+                <div class="name">${urlstringM[i].name}</div>
+            </div>
+        </div>
+    </a>`
+}}
+
+function changeWidth(LorS){
+    if(LorS==1){
+        $('.bar').css('width','140px')
+        $('.bar').css('flex-direction','column')
+        $('.iconIMG').css('height','80px')
+        $('.name').css('font-size','16px')
+        $('.name').css('text-align','center')
+        $('.name').css('margin-left','0')
+        $('.name').addClass('buhanhang')
+    }
+    else if(LorS==2){
+        $('.bar').css('width','350px')
+        $('.bar').css('flex-direction','row')
+        $('.iconIMG').css('height','100%')
+        $('.name').css('font-size','22px')
+        $('.name').css('text-align','center')
+        $('.name').css('margin-left','20px')
+        $('.name').removeClass('buhanhang')
+    }
+}
